1aa4229588ea1b42e131b19cc7f3006aa389ce4a
MedBouhdida <bouhdidamohamed22@gmail.com>
2022-12-21 23:06:47 +0100

    Initial commit

---
+++ BackGaming/Controllers/ClientController.cs
@@ -0,0 +1,58 @@
+﻿using BackGaming.Data;
+using BackGaming.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Microsoft.Extensions.ObjectPool;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BackGaming.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController:Controller
+    {
+
+        private readonly GamingApiDbContext dbContext;
+
+        public ClientController(GamingApiDbContext dbContext) 
+            {
+                this.dbContext = dbContext;
+            }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClients()
+        {
+            return Ok(await dbContext.Client.ToListAsync());
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> addClient(Client client)
+        {
+
+            var cl = dbContext.Client.Where(x => x.Email == client.Email).FirstOrDefault();
+            if(cl == null)
+            {
+                byte[] enteredPasswordBytes = Encoding.UTF8.GetBytes(client.Password);
+                byte[] enteredHashBytes = SHA256.Create().ComputeHash(enteredPasswordBytes);
+                string enteredHashedPassword = Convert.ToBase64String(enteredHashBytes);
+                client.Password = enteredHashedPassword;
+                await dbContext.Client.AddAsync(client);
+                await dbContext.SaveChangesAsync();
+                return Ok(client);
+            }
+            return Problem("Email exist");
+         
+        }
+
+
+       
+
+        
+    }
+}
---
+++ BackGaming/Data/GamingApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BackGaming.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace BackGaming.Data
+{
+
+    public class GamingApiDbContext : DbContext
+    {
+        public GamingApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Client> Client { get; set; }
+    }
+}
