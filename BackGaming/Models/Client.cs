3cd00c3ab0f281c0a13d555193e84008b41aa283
MedBouhdida <bouhdidamohamed22@gmail.com>
2022-12-24 22:40:13 +0100

    Register & login client

--- BackGaming/Models/Client.cs
+++ BackGaming/Models/Client.cs
@@ -7,13 +7,6 @@ namespace BackGaming.Models
     {
 
 
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; } 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
 
     }
 }
