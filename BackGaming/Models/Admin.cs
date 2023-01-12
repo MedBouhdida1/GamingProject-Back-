bc0e020f458db15baa9d06f0c3e812bbaba1474d
ngGenerateUsername <mbenhamouda099@gmail.com>
2023-01-12 20:30:16 +0100

    add admin

--- BackGaming/Models/Admin.cs
+++ BackGaming/Models/Admin.cs
@@ -7,6 +7,7 @@ namespace BackGaming.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
 
