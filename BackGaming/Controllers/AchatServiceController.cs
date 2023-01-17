2677ef43bc2ac1488e47f6effefe7a9591779536
jlassi aymen <aymentunis08@gmail.com>
2023-01-17 21:58:43 +0100

    create achat service controller

---
+++ BackGaming/Controllers/AchatServiceController.cs
@@ -0,0 +1,30 @@
+﻿using BackGaming.Data;
+using BackGaming.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackGaming.Controllers
+{
+    [ApiController]
+    [Route("api/client")]
+    public class AchatServiceController : Controller
+    {
+        private readonly GamingApiDbContext dbContext;
+        public AchatServiceController(GamingApiDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> addAchatService([FromBody] AchatService achatService)
+        {
+
+                await dbContext.AchatService.AddAsync(achatService);
+                await dbContext.SaveChangesAsync();
+                return Ok(client);
+         
+            return Unauthorized("Email exist");
+
+        }
+
+    }
+}
