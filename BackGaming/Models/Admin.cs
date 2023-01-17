4595a4bdac6f24482372df25eb707d72b9ff63af
Anas <Anas.gara@sesame.com.tn>
2023-01-17 17:23:33 +0100

    changes

--- BackGaming/Models/Admin.cs
+++ BackGaming/Models/Admin.cs
@@ -7,7 +7,6 @@ namespace BackGaming.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
 
